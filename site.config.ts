bab8af8b9202884167319671d709e96b0c0ddd40
zé <61306556+psilosamples@users.noreply.github.com>
2022-08-13 05:27:10 -0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,20 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7656f3502f0a4b198870d99edc84cd0a',
+  rootNotionPageId: 'e96c38f5e78047018894ba53c70fa335',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'bog bot test',
-
-  author: 'bot test',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
