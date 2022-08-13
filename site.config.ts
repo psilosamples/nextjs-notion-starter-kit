df95aef34cffc5620915e4f048ffeb264220f0cd
zé <61306556+psilosamples@users.noreply.github.com>
2022-08-13 03:08:24 -0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,19 +9,13 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'bog bot test',
+
+  author: 'bot test',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
